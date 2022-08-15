23553603da2ede9ea490d46628f4f67372ef8abf
vladtarnovskiy <vladtarnovski989@gmail.com>
2022-08-15 21:17:32 +0300

    feat: class guessing game

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,25 @@
 class GuessingGame {
-    constructor() {}
-
+    constructor() {
+    }
+ 
     setRange(min, max) {
-
+        this.min = min
+        this.max = max
     }
-
+ 
     guess() {
-
+        this.x =  Math.round((this.min + this.max)/2)
+        return this.x
+       
     }
-
+ 
     lower() {
-
+        this.max = this.x
     }
-
+ 
     greater() {
-
+        this.min = this.x
     }
 }
-
+ 
 module.exports = GuessingGame;
